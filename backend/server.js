619c0a14a97b5a2804c49f71f64ff29dd6e2bada
jamesrhoey <jamesrhoeydecastro7@gmail.com>
2024-11-15 11:36:11 +0800

    activity4

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+
+const express = require('express')
+
+const mongoose = require('mongoose')
+const workoutRoutes = require('./routes/workouts')
+
+//express app
+const app = express()
+
+//middleware
+app.use(express.json())
+
+app.use((req, res, next) =>{
+    console.log(req.path, req.method)
+    next()
+})
+
+
+//routes
+app.use('/api/workouts', workoutRoutes)
+
+
+//connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() =>{
+        app.listen(process.env.PORT, () => {
+            console.log('Listening in port', process.env.PORT)
+        })
+    })
+    .catch((error) =>{
+        console.log(error)
+    })
+
+
+
