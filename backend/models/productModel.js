a4018f3859ba6840aa8fd582f1fd12260edcae02
jamesrhoey <jamesrhoeydecastro7@gmail.com>
2024-11-20 11:26:40 +0800

    act

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const productSchema = new Schema({
-    name:{
+    productName:{
         type: String,
         required: true
     },
--- front/assets/script/fetchProduct.js
+++ front/assets/script/fetchProduct.js
@@ -11,24 +11,32 @@ const createCard =(menuItem) =>{
     const col = document.createElement('div');
     const card = document.createElement('div');
 
-    col.classList.add('')
+    col.classList.add('menu', 'col-sm-4','mb-4');
+    card.classList.add('card', 'menu-item', 'product-card');
+
+    card.append(createCardBody(menuItem.productName, menuItem.price, menuItem.ingredients));
+    card.append(createCardFooter());
+
+    col.append(card);
+
+    return col;
 }
 
 
 
 
-const createCardBody = (name, price, ingredients) =>{
+const createCardBody = (productName, price, ingredients) =>{
     const div = document.createElement('div');
     const h5 = document.createElement('h5');
     const h6 = document.createElement('h6');
     const p = document.createElement('p');
 
-    div.classList.add('card-body',' text-center');
-    h5.classList.add('card-title ','text-white');
-    h6.classList.add('card-text ',' text-white');
-    p.classList.add('card-text',' text-white');
+    div.classList.add('card-body', 'text-center');
+    h5.classList.add('card-title', 'text-white');
+    h6.classList.add('card-text', 'text-white');
+    p.classList.add('card-text', 'text-white');
 
-    h5.innerHTML = name;
+    h5.innerHTML = productName;
     h6.innerHTML = price;
     p.innerText = ingredients;
 
@@ -49,8 +57,8 @@ const createCardFooter = () =>{
     const button2 = document.createElement('button');
 
     div.classList.add('d-flex', 'justify-content-around');
-    button.classList.add('btn',' btn-danger',' delete-btn');
-    button2.classList.add('btn',' btn-warning ','update-btn');
+    button.classList.add('btn', 'btn-danger', 'delete-btn');
+    button2.classList.add('btn', 'btn-warning', 'update-btn');
 
     div.append(button);
     div.append(button2);
